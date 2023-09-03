a09510a7012778351fea5caca61a3e222c0743e0
b00nw33 <33589369+b00nw33@users.noreply.github.com>
2023-09-03 23:01:48 +1000

    stprofilereport

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-from streamlit_pandas_profiling import st_profile_report
+# from streamlit_pandas_profiling import st_profile_report
 import pandas_profiling
 
 # import plotly.express as px
@@ -32,5 +32,5 @@ if choice == "Upload":
 if choice == "Profiling":
     st.title("Exploratory Data Analysis")
     profile_df = df.profile_report()
-    st_profile_report(profile_df)
+    # st_profile_report(profile_df)
 
