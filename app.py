1c2f190ca473d677bf6c654d6e17e51c77143c21
b00nw33 <33589369+b00nw33@users.noreply.github.com>
2023-09-03 23:08:18 +1000

    profiling

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-# from streamlit_pandas_profiling import st_profile_report
-# import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
+import pandas_profiling
 
 # import plotly.express as px
 #from pycaret.regression import setup, compare_models, pull, save_model, load_model
@@ -16,7 +16,7 @@ else:
 
 with st.sidebar:
     st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
-    st.title("OperationalML")
+    st.title("Autopycaret")
     choice = st.radio("Navigation", ["Upload","Profiling","Modelling", "Download"])
     st.info("This project application helps you build and explore your data.")
 
