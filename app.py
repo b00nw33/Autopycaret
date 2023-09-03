2e02b198202199a849dd358d2a126509b4ed02f2
b00nw33 <33589369+b00nw33@users.noreply.github.com>
2023-09-03 23:03:07 +1000

    pandas

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # from streamlit_pandas_profiling import st_profile_report
-import pandas_profiling
+# import pandas_profiling
 
 # import plotly.express as px
 #from pycaret.regression import setup, compare_models, pull, save_model, load_model
