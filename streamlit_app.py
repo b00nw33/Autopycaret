417a87c5230b4e4d37567330a00b084a5c49ae19
b00nw33 <33589369+b00nw33@users.noreply.github.com>
2023-09-04 10:43:17 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,18 +34,12 @@ if choice == "Profiling":
 if choice == "Modelling":
     chosen_target = st.selectbox('Choose the Target Column', df.columns)
     if chosen_target and st.button('Run Modelling'):
-        break
-        classf = setup(data=df, target=chosen_target, train_size = 0.8, normalize = True, session_id = 123)
-        # setup_df = pull()
-        
+        classf = setup(data=df, target=chosen_target, train_size=0.8, normalize=True, session_id=123)
         best_model = compare_models()
-        # compare_df = pull()
         save_model(best_model, 'best_model')
         st.dataframe(compare_df)
 
-
 if choice == "Download":
-    pass
     if os.path.exists('best_model.pkl'):
         with open('best_model.pkl', 'rb') as f:
             st.download_button('Download Model', f, file_name="best_model.pkl")
